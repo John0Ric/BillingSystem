0e84e015324de54f291b427915baa01c97e57450
John0Ric <johnricmadrid.dejesus25@bicol-u.edu.ph>
2026-08-28 15:39:15 +0800

    Add project files.

---
+++ BillingSystem/AddCustomerForm.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace BillingSystem
+{
+    partial class AddCustomerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTitle = new Label();
+            lblFullName = new Label();
+            lblAddress = new Label();
+            lblContact = new Label();
+            lblEmail = new Label();
+            lblBalance = new Label();
+            txtFullName = new TextBox();
+            txtAddress = new TextBox();
+            txtContact = new TextBox();
+            txtEmail = new TextBox();
+            txtBalance = new TextBox();
+            btnSave = new Button();
+            btnClear = new Button();
+            btnBack = new Button();
+            SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.Location = new Point(140, 37);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(157, 21);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "Add New Customer";
+            // 
+            // lblFullName
+            // 
+            lblFullName.AutoSize = true;
+            lblFullName.Location = new Point(99, 92);
+            lblFullName.Name = "lblFullName";
+            lblFullName.Size = new Size(64, 15);
+            lblFullName.TabIndex = 1;
+            lblFullName.Text = "Full Name:";
+            // 
+            // lblAddress
+            // 
+            lblAddress.AutoSize = true;
+            lblAddress.Location = new Point(99, 128);
+            lblAddress.Name = "lblAddress";
+            lblAddress.Size = new Size(52, 15);
+            lblAddress.TabIndex = 2;
+            lblAddress.Text = "Address:";
+            // 
+            // lblContact
+            // 
+            lblContact.AutoSize = true;
+            lblContact.Location = new Point(99, 173);
+            lblContact.Name = "lblContact";
+            lblContact.Size = new Size(99, 15);
+            lblContact.TabIndex = 3;
+            lblContact.Text = "Contact Number:";
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Location = new Point(99, 213);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(39, 15);
+            lblEmail.TabIndex = 4;
+            lblEmail.Text = "Email:";
+            // 
+            // lblBalance
+            // 
+            lblBalance.AutoSize = true;
+            lblBalance.Location = new Point(99, 251);
+            lblBalance.Name = "lblBalance";
+            lblBalance.Size = new Size(83, 15);
+            lblBalance.TabIndex = 5;
+            lblBalance.Text = "Initial Balance:";
+            // 
+            // txtFullName
+            // 
+            txtFullName.Location = new Point(219, 94);
+            txtFullName.Name = "txtFullName";
+            txtFullName.Size = new Size(100, 23);
+            txtFullName.TabIndex = 6;
+            txtFullName.TextChanged += textBox1_TextChanged;
+            // 
+            // txtAddress
+            // 
+            txtAddress.Location = new Point(219, 128);
+            txtAddress.Name = "txtAddress";
+            txtAddress.Size = new Size(100, 23);
+            txtAddress.TabIndex = 7;
+            // 
+            // txtContact
+            // 
+            txtContact.Location = new Point(219, 165);
+            txtContact.Name = "txtContact";
+            txtContact.Size = new Size(100, 23);
+            txtContact.TabIndex = 8;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(219, 205);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(100, 23);
+            txtEmail.TabIndex = 9;
+            // 
+            // txtBalance
+            // 
+            txtBalance.Location = new Point(219, 243);
+            txtBalance.Name = "txtBalance";
+            txtBalance.Size = new Size(100, 23);
+            txtBalance.TabIndex = 10;
+            txtBalance.TextChanged += textBox5_TextChanged;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(99, 301);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(75, 23);
+            btnSave.TabIndex = 11;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(180, 301);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(75, 23);
+            btnClear.TabIndex = 12;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            btnBack.Location = new Point(261, 301);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(75, 23);
+            btnBack.TabIndex = 13;
+            btnBack.Text = "Back";
+            btnBack.UseVisualStyleBackColor = true;
+            // 
+            // AddCustomerForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(434, 381);
+            Controls.Add(btnBack);
+            Controls.Add(btnClear);
+            Controls.Add(btnSave);
+            Controls.Add(txtBalance);
+            Controls.Add(txtEmail);
+            Controls.Add(txtContact);
+            Controls.Add(txtAddress);
+            Controls.Add(txtFullName);
+            Controls.Add(lblBalance);
+            Controls.Add(lblEmail);
+            Controls.Add(lblContact);
+            Controls.Add(lblAddress);
+            Controls.Add(lblFullName);
+            Controls.Add(lblTitle);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "AddCustomerForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Billing System - Add Customer";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblTitle;
+        private Label lblFullName;
+        private Label lblAddress;
+        private Label lblContact;
+        private Label lblEmail;
+        private Label lblBalance;
+        private TextBox txtFullName;
+        private TextBox txtAddress;
+        private TextBox txtContact;
+        private TextBox txtEmail;
+        private TextBox txtBalance;
+        private Button btnSave;
+        private Button btnClear;
+        private Button btnBack;
+    }
+}
---
+++ BillingSystem/LoginForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace BillingSystem
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTitle = new Label();
+            lblPassword = new Label();
+            lblUsername = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            btnCancel = new Button();
+            SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.Location = new Point(106, 23);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(163, 25);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "BILLING SYSTEM";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(51, 117);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(60, 15);
+            lblPassword.TabIndex = 1;
+            lblPassword.Text = "Password:";
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Location = new Point(51, 75);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(63, 15);
+            lblUsername.TabIndex = 2;
+            lblUsername.Text = "Username:";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(129, 75);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(100, 23);
+            txtUsername.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(129, 109);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(100, 23);
+            txtPassword.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(63, 170);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(75, 23);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(154, 170);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 23);
+            btnCancel.TabIndex = 6;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(384, 261);
+            Controls.Add(btnCancel);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(lblUsername);
+            Controls.Add(lblPassword);
+            Controls.Add(lblTitle);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "LoginForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Billing System - Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblTitle;
+        private Label lblPassword;
+        private Label lblUsername;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private Button btnCancel;
+    }
+}
